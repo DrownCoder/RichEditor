badbfe3bfc5d7a73054e87d2c614e0eddfe9f43c
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2018-02-08 20:39:18 +0800

    删除算法一

--- editor/src/main/java/com/study/xuan/editor/adapter/RichAdapter.java
+++ editor/src/main/java/com/study/xuan/editor/adapter/RichAdapter.java
@@ -20,6 +20,7 @@ import android.widget.ImageView;
 import com.study.xuan.editor.R;
 import com.study.xuan.editor.model.RichModel;
 import com.study.xuan.editor.model.SpanModel;
+import com.study.xuan.editor.operate.filter.SpanInputFilter;
 import com.study.xuan.editor.operate.paragraph.ParagraphHelper;
 import com.study.xuan.editor.operate.span.MultiSpannableString;
 import com.study.xuan.shapebuilder.shape.ShapeBuilder;
@@ -54,7 +55,6 @@ public class RichAdapter extends RecyclerView.Adapter {
 
     private ParagraphHelper paragraphHelper;
 
-    private boolean isNotify;
     public boolean isEnter;
 
     public void addHeaderView(View header) {
@@ -343,21 +343,20 @@ public class RichAdapter extends RecyclerView.Adapter {
         if (mData.size() == 1 && mData.get(0).type == TYPE_EDIT) {
             mData.get(0).hint = DEFAULT_HINT;
         }
-        isNotify = true;
         notifyDataSetChanged();
     }
 
     private class EditHolder extends RecyclerView.ViewHolder {
         private CustomEditTextListener textWatcher;
         private EditText mEt;
-        private CustomInputFilter filter;
+        private SpanInputFilter filter;
 
         EditHolder(View itemView) {
             super(itemView);
             mEt = (EditText) itemView;
             mEt.setOnClickListener(onClickListener);
             textWatcher = new CustomEditTextListener();
-            filter = new CustomInputFilter();
+            filter = new SpanInputFilter(mData);
             mEt.addTextChangedListener(textWatcher);
             mEt.setOnKeyListener(onKeyListener);
             mEt.setFilters(new InputFilter[]{filter});
@@ -402,117 +401,6 @@ public class RichAdapter extends RecyclerView.Adapter {
     private class HeadHolder extends RecyclerView.ViewHolder {
         HeadHolder(View mHeader) {
             super(mHeader);
-        }
-    }
-
-    private class CustomInputFilter implements InputFilter {
-        private int position;
-        private List<SpanModel> spanModels;
-        private RichModel richModel;
-        private MultiSpannableString spannableString;
-        //当前span样式
-        private SpanModel nowSpanModel;
-        boolean isDelete;//增加文字还是删除文字
-
-        public CustomInputFilter() {
-            spannableString = new MultiSpannableString();
-        }
-
-        void updatePosition(int position) {
-            this.position = position;
-            richModel = mData.get(position);
-            spanModels = richModel.getSpanList();
-        }
-
-        /**
-         * @param charSequence 即将输入的字符串
-         * @param start        source的start
-         * @param end          source的end start为0，end也可理解为source长度
-         * @param dest         dest输入框中原来的内容
-         * @param dstart       要替换或者添加的起始位置，即光标所在的位置
-         * @param dend         要替换或者添加的终止始位置，若为选择一串字符串进行更改，则为选中字符串 最后一个字符在dest中的位置
-         */
-        @Override
-        public CharSequence filter(CharSequence charSequence, int start, int end, Spanned dest, int
-                dstart, int dend) {
-            Log.i(BASE_LOG, "char:" + charSequence + "-" + start + "-" + end + "-" + dest + "-" +
-                    dstart + "-" + dend);
-            isDelete = TextUtils.isEmpty(charSequence);
-            if (isNotify) {
-                isNotify = false;
-                spannableString.clear();
-                spannableString.clearSpans();
-                spannableString.append(charSequence);
-                Log.i(BASE_LOG, spannableString.toString());
-                for (SpanModel model : spanModels) {
-                    Log.i(BASE_LOG, model.mSpans + "start:" + model.start + "end:" + model.end);
-                    spannableString.setMultiSpans(model.mSpans, model.start, model.end, Spanned
-                            .SPAN_EXCLUSIVE_EXCLUSIVE);
-                }
-                return spannableString;
-            }
-            //输入后要做两步，1.保存新的样式2.设置新的样式
-            int i = 0;
-            if (richModel.isNewSpan) {
-                //使用过后，变成false
-                richModel.isNewSpan = false;
-                //新建span样式
-                nowSpanModel = richModel.newSpan;
-                if (dstart == richModel.source.length()) {
-                    //光标在文段末尾
-                    nowSpanModel.start = dstart;
-                    nowSpanModel.end = dstart + end - start;
-                    spanModels.add(nowSpanModel);
-                } else {
-                    //光标在文中
-                    for (; i < spanModels.size(); i++) {
-                        SpanModel spanModel = spanModels.get(i);
-                        if (start > spanModel.end) {
-                            continue;
-                        }
-                        if (start > spanModel.start && start < spanModel.end) {
-                            //光标位置类似：“这是一段文字|光标在中间”
-                            spanModel.end = start;
-                            nowSpanModel.start = start;
-                            nowSpanModel.end = start + end - start;
-                            spanModels.add(i, nowSpanModel);
-                            break;
-                        }
-
-                    }
-                }
-            } else {
-                //保持原有span样式，不进行插入操作
-                for (; i < spanModels.size(); i++) {
-                    SpanModel spanModel = spanModels.get(i);
-                    if (dstart > spanModel.end) {
-                        //光标在一段文字末尾修改，则到下一个span
-                        continue;
-                    }
-                    if (dstart > spanModel.start && dstart <= spanModel.end) {
-                        //光标位置类似：“这是一段文字|光标在中间”
-                        nowSpanModel = spanModel;
-                        spanModel.end += end - start;
-                        i++;
-                        break;
-                    }
-                }
-                for (; i < spanModels.size(); i++) {
-                    spanModels.get(i).start += end - start;
-                    spanModels.get(i).end += end - start;
-                }
-            }
-            spannableString.clear();
-            spannableString.clearSpans();
-            spannableString.append(charSequence);
-            if (nowSpanModel != null) {
-                for (SpanModel model : spanModels) {
-                    Log.i(BASE_LOG, model.mSpans + "start:" + model.start + "end:" + model.end);
-                }
-                spannableString.setMultiSpans(nowSpanModel.mSpans, 0, charSequence.length(), Spanned
-                        .SPAN_EXCLUSIVE_INCLUSIVE);
-            }
-            return spannableString;
         }
     }
 
---
+++ editor/src/main/java/com/study/xuan/editor/operate/filter/SpanInputFilter.java
@@ -0,0 +1,163 @@
+package com.study.xuan.editor.operate.filter;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.study.xuan.editor.model.RichModel;
+import com.study.xuan.editor.model.SpanModel;
+import com.study.xuan.editor.operate.span.MultiSpannableString;
+
+import java.util.List;
+
+import static com.study.xuan.editor.common.Const.BASE_LOG;
+
+public class SpanInputFilter implements InputFilter {
+    private List<SpanModel> spanModels;
+    private RichModel richModel;
+    private MultiSpannableString spannableString;
+    //当前span样式
+    private SpanModel nowSpanModel;
+    private boolean isNotify;
+    private List<RichModel> mData;
+
+    public SpanInputFilter(List<RichModel> data) {
+        spannableString = new MultiSpannableString();
+        mData = data;
+    }
+
+    public void updatePosition(int position) {
+        richModel = mData.get(position);
+        spanModels = richModel.getSpanList();
+        isNotify = true;
+    }
+
+    /**
+     * @param charSequence 即将输入的字符串
+     * @param start        source的start
+     * @param end          source的end start为0，end也可理解为source长度
+     * @param dest         dest输入框中原来的内容
+     * @param dstart       要替换或者添加的起始位置，即光标所在的位置
+     * @param dend         要替换或者添加的终止始位置，若为选择一串字符串进行更改，则为选中字符串 最后一个字符在dest中的位置
+     */
+    @Override
+    public CharSequence filter(CharSequence charSequence, int start, int end, Spanned dest, int
+            dstart, int dend) {
+        Log.i(BASE_LOG, "char:" + charSequence + "-" + start + "-" + end + "-" + dest + "-" +
+                dstart + "-" + dend);
+        //判断是否是删除
+        boolean isDelete = TextUtils.isEmpty(charSequence);
+        if (isNotify) {
+            isNotify = false;
+            spannableString.clear();
+            spannableString.clearSpans();
+            spannableString.append(charSequence);
+            Log.i(BASE_LOG, spannableString.toString());
+            for (SpanModel model : spanModels) {
+                Log.i(BASE_LOG, model.mSpans + "start:" + model.start + "end:" + model.end);
+                spannableString.setMultiSpans(model.mSpans, model.start, model.end, Spanned
+                        .SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+            return spannableString;
+        }
+        //输入后要做两步，1.保存新的样式2.设置新的样式
+        int i = 0;
+        if (richModel.isNewSpan) {
+            //使用过后，变成false
+            richModel.isNewSpan = false;
+            //新建span样式
+            nowSpanModel = richModel.newSpan;
+            if (dstart == richModel.source.length()) {
+                //光标在文段末尾
+                nowSpanModel.start = dstart;
+                nowSpanModel.end = dstart + end - start;
+                spanModels.add(nowSpanModel);
+            } else {
+                //光标在文中
+                for (; i < spanModels.size(); i++) {
+                    SpanModel spanModel = spanModels.get(i);
+                    if (start > spanModel.end) {
+                        continue;
+                    }
+                    if (start > spanModel.start && start < spanModel.end) {
+                        //光标位置类似：“这是一段文字|光标在中间”
+                        spanModel.end = start;
+                        nowSpanModel.start = start;
+                        nowSpanModel.end = start + end - start;
+                        spanModels.add(i, nowSpanModel);
+                        break;
+                    }
+
+                }
+            }
+        } else {
+            if (isDelete) {
+                sortOnDelete(dstart, dend);
+                return charSequence;
+            }
+            //保持原有span样式，不进行插入操作
+            for (; i < spanModels.size(); i++) {
+                SpanModel spanModel = spanModels.get(i);
+                if (dstart > spanModel.end) {
+                    //光标在一段文字末尾修改，则到下一个span
+                    continue;
+                }
+                if (dstart > spanModel.start && dstart <= spanModel.end) {
+                    //光标位置类似：“这是一段文字|光标在中间”
+                    nowSpanModel = spanModel;
+                    spanModel.end += end - start;
+                    i++;
+                    break;
+                }
+            }
+            for (; i < spanModels.size(); i++) {
+                spanModels.get(i).start += end - start;
+                spanModels.get(i).end += end - start;
+            }
+        }
+        spannableString.clear();
+        spannableString.clearSpans();
+        spannableString.append(charSequence);
+        if (nowSpanModel != null) {
+            //logModel();
+            spannableString.setMultiSpans(nowSpanModel.mSpans, 0, charSequence.length(), Spanned
+                    .SPAN_EXCLUSIVE_INCLUSIVE);
+        }
+        return spannableString;
+    }
+
+    /**
+     * 删除，重新排列数据算法
+     */
+    private void sortOnDelete(int start, int end) {
+        int size = end - start;
+        for (int i = 0; i < spanModels.size(); i++) {
+            SpanModel model = spanModels.get(i);
+            if (start >= model.start && start <= model.end) {
+                while (i < spanModels.size() - 1 && end > model.end) {
+                    spanModels.remove(i + 1);
+                    model = spanModels.get(i + 1);
+                }
+                for (; i < spanModels.size(); i++) {
+                    SpanModel item = spanModels.get(i);
+                    if (item.start > start) {
+                        item.start -= size;
+                    }
+                    item.end -= size;
+                    if (item.start == item.end) {
+                        spanModels.remove(i);
+                    }
+                }
+                logModel();
+                return;
+            }
+        }
+    }
+
+    private void logModel() {
+        for (SpanModel item : spanModels) {
+            Log.i(BASE_LOG, item.mSpans + "start:" + item.start + "end:" + item.end);
+        }
+    }
+}
